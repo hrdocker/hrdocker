4b5ac35edc3869bd9a333748da9d46c15d62bb51
Urvashi Reddy <urvashi.reddy@gmail.com>
2014-11-14 23:40:33 -0800

    add correct replacement word in deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Pulling image from DockerHub"
-docker pull dockerHubRepo:latest
+docker pull dockerRepoName:latest
 
 echo "Starting a mongo container"
 docker run --restart=always -d --name db mongo:latest
@@ -9,7 +9,7 @@ docker run --restart=always -d --name db mongo:latest
 echo "Creating application container"
 echo "Linking to mongo container"
 echo "Running application script"
-docker run --restart=always -i -t -p 80:9000 --link db:dbLink dockerHubRepo:latest sh prod/src/app.sh
+docker run --restart=always -i -t -p 80:9000 --link db:dbLink dockerRepoName:latest sh prod/src/app.sh
 
 echo "Deployed!"
 # echo "http://Cloud-service-name:80"
